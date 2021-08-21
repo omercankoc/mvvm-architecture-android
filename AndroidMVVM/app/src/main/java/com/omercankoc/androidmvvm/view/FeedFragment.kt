e1a230cb186b1b2bcb624069bb65e89d0d508836
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-08-22 01:05:46 +0300

    Completed MVVM

--- AndroidMVVM/app/src/main/java/com/omercankoc/androidmvvm/view/FeedFragment.kt
+++ AndroidMVVM/app/src/main/java/com/omercankoc/androidmvvm/view/FeedFragment.kt
@@ -5,13 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.omercankoc.androidmvvm.adapter.CountryAdapter
 import com.omercankoc.androidmvvm.databinding.FragmentFeedBinding
+import com.omercankoc.androidmvvm.viewmodel.FeedViewModel
 
 
 class FeedFragment : Fragment() {
 
     // Definition Binding
     private lateinit var binding : FragmentFeedBinding
+
+    private lateinit var feedViewModel : FeedViewModel
+    private val countryAdapter = CountryAdapter(arrayListOf())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,5 +36,21 @@ class FeedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        feedViewModel = ViewModelProviders.of(this).get(FeedViewModel::class.java)
+        feedViewModel.refreshFeedData()
+
+        binding.recyclerViewCountries.layoutManager = LinearLayoutManager(context)
+        binding.recyclerViewCountries.adapter = countryAdapter
+
+        observeLiveData()
+    }
+
+    fun observeLiveData(){
+        feedViewModel.countryMutableLiveData.observe(viewLifecycleOwner, Observer { countries ->
+            countries?.let {
+                countryAdapter.refresh(countries)
+            }
+        })
     }
 }
