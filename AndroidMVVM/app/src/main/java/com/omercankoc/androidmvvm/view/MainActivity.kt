2231d2068101422592911df8af8f2c70135fea41
Ömer Can Koç <63169919+omercankoc@users.noreply.github.com>
2021-08-21 20:09:03 +0300

    Created UIs

--- AndroidMVVM/app/src/main/java/com/omercankoc/androidmvvm/view/MainActivity.kt
+++ AndroidMVVM/app/src/main/java/com/omercankoc/androidmvvm/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.omercankoc.androidmvvm
+package com.omercankoc.androidmvvm.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.omercankoc.androidmvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
